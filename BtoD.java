bf1f5d8b10406114c99335612b8e1e5dadd0d1e4
RamyaRSC <111015805+RamyaRSC@users.noreply.github.com>
2023-03-11 11:40:18 +0530

    Update BtoD.java

--- BtoD.java
+++ BtoD.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 public class BtoD{
 	public static void main(String[]args){
 		Scanner sn=new Scanner(System.in);
-		System.out.println("enter the decimal no:");
+		System.out.println("enter decimal no:");
 		int b1=sn.nextInt();
-		String b2=Integer.toBinaryString(b1);
-		System.out.println("integer to binary conversion:"+ b2);
+		System.out.println("integer to binary conversion:"+ Integer.toBinaryString(b1));
+		
 		System.out.println("enter a binary no:");
 		String b3=sn.nextLine();
-		int a=Integer.parseInt(b3,2);
-		System.out.println("binary to decimal conversion:"+a);
+		System.out.println("binary to decimal conversion:"+Integer.parseInt(b3,2));
 	}
 }
